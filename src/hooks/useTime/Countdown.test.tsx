48d4b63a326870788eff084e9e46001a3d67d5b1
James Fulford <james.fulford@outlook.com>
2019-11-24 22:55:49 -0500

    WIP(Countdown): Stability and explanation

--- src/hooks/useTime/Countdown.test.tsx
+++ src/hooks/useTime/Countdown.test.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import { Countdown } from './Countdown';
 import { DateTime } from 'luxon';
+import { useFakeTimers, SinonFakeTimers } from 'sinon';
+
+import { Countdown } from './Countdown';
 
 describe('Countdown', () => {
+
+    // For time stability, control time
+    let clock: SinonFakeTimers;
+    beforeEach(() => { clock = useFakeTimers(); });
+    afterEach(() => { clock.restore(); });
+
     it('should match snapshot', () => {
         const end = DateTime.local().plus({ seconds: 1.1 });
         const s = renderer.create(<Countdown end={end} />);
@@ -12,5 +20,19 @@ describe('Countdown', () => {
         expect(s.toJSON()).toMatchSnapshot();
     });
 
-    // TODO(james.fulford): test component time functionality
+    // Even if I tested actual time functionality of Countdown,
+    // I would be testing the particular usage of useTime in Countdown.
+    // This makes actual testing of useTime particularly difficult
+    // especially if there are unused return values or options.
+
+    // (you could wire up the options to be passable by props,
+    // as I have done for Countdown with the _useTimeOptions prop,
+    // there still isn't a way to test the return values of useTime)
+
+    // Trying to test the time object returned is difficult
+    // primarily because the only way to access it in this component
+    // is through the result of `.toFormat('hh:mm:ss')`, which doesn't
+    // change by the millisecond.
+
+    // Implementation shouldn't get in the way of testing a reusable hook.
 });
